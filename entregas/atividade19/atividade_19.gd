f99477d0c2c76eeb5d744e0e44a1bd08b197c273
belafukase <isabelafukasehp@gmail.com>
2025-09-23 11:41:15 -0300

    19

---
+++ entregas/atividade19/atividade_19.gd
@@ -0,0 +1,19 @@
+extends Node2D
+var dseg = 0
+var seg = 50
+var min = 0
+
+
+func _on_timer_timeout() -> void:
+	dseg +=1 
+	$dseg.text = str(dseg)
+	if dseg == 9:
+		seg +=1
+		$seg.text = str(seg)
+		dseg = 0
+		if seg == 59:
+			min +=1
+			$min.text = str(min)
+			seg = 0
+			$seg.text = str(0)
+	pass # Replace with function body.
